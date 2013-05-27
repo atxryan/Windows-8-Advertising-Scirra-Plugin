495d5d368282104a0f8174dededd8732e2f25249
Ryan Joy <atxryan@gmail.com>
2013-05-26 19:38:24 -0500

    Adding license;

--- Plugin/files/win8advertising/edittime.js
+++ Plugin/files/win8advertising/edittime.js
@@ -1,3 +1,5 @@
+// Copyright © Microsoft Corporation.  The use of this code (the “software”) is governed by the Microsoft Sample license terms found at https://github.com/atxryan/Windows-8-Advertising-Scirra-Plugin/blob/master/LICENSE.txt.
+
 function GetPluginSettings() {
 	return {
 		"name":			"Windows 8 Advertising",
--- Plugin/files/win8advertising/runtime.js
+++ Plugin/files/win8advertising/runtime.js
@@ -1,4 +1,6 @@
-﻿// ECMAScript 5 strict mode
+﻿// Copyright © Microsoft Corporation.  The use of this code (the “software”) is governed by the Microsoft Sample license terms found at https://github.com/atxryan/Windows-8-Advertising-Scirra-Plugin/blob/master/LICENSE.txt.
+
+// ECMAScript 5 strict mode
 "use strict";
 
 assert2(cr, "cr namespace not created");
